45b8e633fab1dfeb3e66e5d36a6df23ae0421231
JP <potti.jordan@gmail.com>
2017-07-31 20:24:22 -0500

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -2,12 +2,16 @@ import socket
 import sys
 
 if len(sys.argv) != 2:
-	print "Usage: resolver.py <list of hostnames>"
-	sys.exit(0)
+        print "Usage: resolver.py <list of hostnames>"
+        sys.exit(0)
 s = set()
 with open(sys.argv[1],"r") as text:
-	for line in text:
-		resolved = socket.gethostbyname(line.strip())
-		print resolved
-		with open(sys.argv[1]+".txt","a") as resolved_list:
-			resolved_list.write(resolved+"\n")
+        for line in text:
+                if ('abcdefghijklmnopqrstuvwxyz') not in line:
+                        resolved = socket.getfqdn(line)
+                        print resolved
+                else:
+                        resolved = socket.gethostbyname(line.strip())
+                        print resolved
+                        with open(sys.argv[1]+".txt","a") as resolved_list:
+                                resolved_list.write(resolved+"\n")
